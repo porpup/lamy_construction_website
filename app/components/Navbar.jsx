b2fe66075b9c51bb7659b23049e3ab09b83769da
Oleksandr Shyryayev <porpup@hotmail.com>
2024-06-25 17:01:40 -0400

    fixed warnings in browser

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,42 +6,44 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
-	const basePath = BasePath();
-	const [isScrolled, setIsScrolled] = useState(false);
+  const basePath = BasePath();
+  const [isScrolled, setIsScrolled] = useState(false);
 
-	useEffect(() => {
-		const handleScroll = () => {
-			setIsScrolled(window.scrollY > 10);
-		};
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
 
-		// Set the initial state based on the current scroll position
-		handleScroll();
+    // Set the initial state based on the current scroll position
+    handleScroll();
 
-		window.addEventListener("scroll", handleScroll);
-		return () => {
-			window.removeEventListener("scroll", handleScroll);
-		};
-	}, []);
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
-	return (
-		<div
-			className={`fixed top-0 left-0 w-full z-10 transition-colors duration-300 ${
-				isScrolled ? "bg-stone-800" : "bg-transparent"
-			}`}
-		>
-			<div className="mx-auto flex items-center justify-between p-4">
-				<Link href="/">
-					<Image
-						src={`${basePath}/assets/logo.png`}
-						width={70}
-						height={70}
-						alt="logo"
-					/>
-				</Link>
-				{/* Add other navbar items here */}
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={`fixed top-0 left-0 w-full z-10 transition-colors duration-300 ${
+        isScrolled ? "bg-stone-800" : "bg-transparent"
+      }`}
+    >
+      <div className="mx-auto flex items-center justify-between p-4">
+        <Link href="/">
+          <Image
+            src={`${basePath}/assets/logo.png`}
+            width={70}
+            height={70}
+            alt="logo"
+            priority={true}
+            style={{ width: '70px', height: '70px' }} // Ensuring dimensions are fixed
+          />
+        </Link>
+        {/* Add other navbar items here */}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
