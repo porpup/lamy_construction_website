483dae2eb170a4084def77feac54ee0c7bfbd14c
Oleksandr Shyryayev <porpup@hotmail.com>
2024-06-25 17:06:20 -0400

    fixed warning in browser on Github Pages

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,17 +6,33 @@ const inter = Inter({ subsets: ["latin"] });
 const basePath = BasePath();
 
 export const metadata = {
-	title: "Lamy Construction",
-	description: "Lamy Construction Website",
-	icons: {
-		icon: `${basePath}/assets/logo.png`,
-	},
+  title: "Lamy Construction",
+  description: "Lamy Construction Website",
+  icons: {
+    icon: `${basePath}/assets/logo.png`,
+  },
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="preload"
+          href="https://fonts.gstatic.com/s/rubik/v28/iJWKBXyIfDnIV7nBrXyw023e.woff2"
+          as="font"
+          type="font/woff2"
+          crossorigin="anonymous"
+        />
+        <link
+          rel="icon"
+          href={`${basePath}/assets/logo.png`}
+          type="image/png"
+        />
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
