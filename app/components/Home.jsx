935923f22b3510276f6de852aedc5f6a03b26ff3
Oleksandr Shyryayev <porpup@hotmail.com>
2024-06-27 00:18:59 -0400

    fixed text size for vision

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -16,11 +16,11 @@ const Home = () => {
         className="w-full"
       />
       {language === 'en' ? (
-        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-stone-800 whitespace-nowrap drop-shadow-lg vision-text">
+        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-semibold text-stone-800 whitespace-nowrap drop-shadow-lg vision-text">
           Precision. Reliability. Excellence.
         </p>
       ) : (
-        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-stone-800 whitespace-nowrap drop-shadow-lg vision-text">
+        <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-semibold text-stone-800 whitespace-nowrap drop-shadow-lg vision-text">
           Précision. Fiabilité. Excellence.
         </p>
       )}
