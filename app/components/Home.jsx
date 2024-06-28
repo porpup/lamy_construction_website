8b79341c3d39b707476640bb3ed499c8d46da6cf
Oleksandr Shyryayev <porpup@hotmail.com>
2024-06-27 20:15:32 -0400

    fixed text height in Home component

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -19,11 +19,11 @@ const Home = () => {
 				style={{ width: "100%", height: "auto" }}
 			/>
 			{language === "en" ? (
-				<p className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-100 whitespace-nowrap drop-shadow-lg">
+				<p className="absolute top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-100 whitespace-nowrap drop-shadow-lg">
 					Precision. Reliability. Excellence.
 				</p>
 			) : (
-				<p className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-100 whitespace-nowrap drop-shadow-lg">
+				<p className="absolute top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-100 whitespace-nowrap drop-shadow-lg">
 					Précision. Fiabilité. Excellence.
 				</p>
 			)}
