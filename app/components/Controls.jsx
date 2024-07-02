d0349d3d433289a61194cb7b2981ec19c9c1ba63
Oleksandr Shyryayev <porpup@hotmail.com>
2024-07-02 19:50:01 -0400

    X in navbar fixing colors

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Controls = ({ isMenuOpen, toggleMenu, isScrolled, isGalleryPage }) => {
-  const lineBaseClass = "block w-full h-0.5 transition-transform duration-200";
-  const colorClass = isScrolled || isGalleryPage
-    ? "bg_gray group-hover:bg-neutral-100"
-    : "bg-neutral-100 group-hover:bg-stone-800";
+	const [isPressed, setIsPressed] = useState(false);
 
-  return (
-    <button className="menuToggle group" onClick={toggleMenu}>
-      <span
-        className={`${lineBaseClass} ${
-          isMenuOpen ? "translate-y-3 rotate-45" : ""
-        } ${colorClass}`}
-      ></span>
-      <span
-        className={`${lineBaseClass} ${
-          isMenuOpen ? "opacity-0" : ""
-        } ${colorClass}`}
-      ></span>
-      <span
-        className={`${lineBaseClass} ${
-          isMenuOpen ? "-translate-y-3 -rotate-45" : ""
-        } ${colorClass}`}
-      ></span>
-    </button>
-  );
+	const handleMouseDown = () => {
+		setIsPressed(true);
+	};
+
+	const handleMouseUp = () => {
+		setIsPressed(false);
+	};
+
+	const lineBaseClass = "block w-full h-0.5 transition-transform duration-200";
+	const colorClass =
+		isScrolled || isGalleryPage
+			? "bg_gray group-hover:bg-neutral-100"
+			: "bg-neutral-100 group-hover:bg-stone-800";
+	const pressedClass = isPressed ? "bg-neutral-100" : "";
+
+	return (
+		<button
+			className="menuToggle group"
+			onClick={toggleMenu}
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}
+			onTouchStart={handleMouseDown}
+			onTouchEnd={handleMouseUp}
+		>
+			<span
+				className={`${lineBaseClass} ${
+					isMenuOpen ? "translate-y-3 rotate-45" : ""
+				} ${colorClass} ${pressedClass}`}
+			></span>
+			<span
+				className={`${lineBaseClass} ${
+					isMenuOpen ? "opacity-0" : ""
+				} ${colorClass} ${pressedClass}`}
+			></span>
+			<span
+				className={`${lineBaseClass} ${
+					isMenuOpen ? "-translate-y-3 -rotate-45" : ""
+				} ${colorClass} ${pressedClass}`}
+			></span>
+		</button>
+	);
 };
 
 export default Controls;
