5588b161272e9e02021e81b679d005ebf1229339
Oleksandr Shyryayev <porpup@hotmail.com>
2024-07-02 19:45:30 -0400

    bugs fixing

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
-const Controls = ({ isMenuOpen, toggleMenu, isScrolled }) => {
-	const lineBaseClass = "block w-full h-0.5 transition-transform duration-200";
-	const colorClass = isScrolled
-		? "bg_gray group-hover:bg-neutral-100 group-active:bg-neutral-100"
-		: "bg-neutral-100 group-hover:bg-stone-800 group-active:bg-stone-800";
+const Controls = ({ isMenuOpen, toggleMenu, isScrolled, isGalleryPage }) => {
+  const lineBaseClass = "block w-full h-0.5 transition-transform duration-200";
+  const colorClass = isScrolled || isGalleryPage
+    ? "bg_gray group-hover:bg-neutral-100"
+    : "bg-neutral-100 group-hover:bg-stone-800";
 
-	return (
-		<button className="menuToggle group" onClick={toggleMenu}>
-			<span
-				className={`${lineBaseClass} ${
-					isMenuOpen ? "translate-y-3 rotate-45" : ""
-				} ${colorClass}`}
-			></span>
-			<span
-				className={`${lineBaseClass} ${
-					isMenuOpen ? "opacity-0" : ""
-				} ${colorClass}`}
-			></span>
-			<span
-				className={`${lineBaseClass} ${
-					isMenuOpen ? "-translate-y-3 -rotate-45" : ""
-				} ${colorClass}`}
-			></span>
-		</button>
-	);
+  return (
+    <button className="menuToggle group" onClick={toggleMenu}>
+      <span
+        className={`${lineBaseClass} ${
+          isMenuOpen ? "translate-y-3 rotate-45" : ""
+        } ${colorClass}`}
+      ></span>
+      <span
+        className={`${lineBaseClass} ${
+          isMenuOpen ? "opacity-0" : ""
+        } ${colorClass}`}
+      ></span>
+      <span
+        className={`${lineBaseClass} ${
+          isMenuOpen ? "-translate-y-3 -rotate-45" : ""
+        } ${colorClass}`}
+      ></span>
+    </button>
+  );
 };
 
 export default Controls;
