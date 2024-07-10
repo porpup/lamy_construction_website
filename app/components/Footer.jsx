b6611c3d17b7e0bf66538b21e5f624e8d79f63ce
Oleksandr Shyryayev <porpup@hotmail.com>
2024-07-10 14:22:34 -0400

    footer small bugs fixed

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -97,25 +97,24 @@ const Footer = () => {
 					<div className="text-center mb-2">
 						<p>{texts.jobTitle}:</p>
 						<p>Robert Giguère</p>
+						<p className="flex items-center hover:text-neutral-100">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 24 24"
+								fill="currentColor"
+								className="mr-3 h-5 w-5"
+							>
+								<path
+									fillRule="evenodd"
+									d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+									clipRule="evenodd"
+								/>
+							</svg>
+							<a href={`tel:${company.phoneNo}`}>{company.phoneNo}</a>
+						</p>
 					</div>
-					<p className="flex items-center hover:text-neutral-100">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							fill="currentColor"
-							className="mr-3 h-5 w-5"
-						>
-							<path
-								fillRule="evenodd"
-								d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
-								clipRule="evenodd"
-							/>
-						</svg>
-						<a href={`tel:${company.phoneNo}`}>{company.phoneNo}</a>
-					</p>
 				</div>
 			</div>
-
 			<hr className="h-0.5 mx-auto my-4 border-0 rounded md:my-5 bg_gray" />
 			<div className="pb-5 text-center flex flex-col items-center md:flex-row md:justify-center md:space-x-16">
 				<p className="mb-2 md:mb-0">Copyright © 2024 - {company.name}</p>
