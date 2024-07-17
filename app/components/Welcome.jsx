a9da1d45026f1f215d70b733cf83e7e989332a3f
Oleksandr Shyryayev <porpup@hotmail.com>
2024-07-17 13:30:35 -0400

    small bug fixed

--- app/components/Welcome.jsx
+++ app/components/Welcome.jsx
@@ -15,6 +15,7 @@ const Welcome = () => {
 				width={1920}
 				height={1080}
 				style={{ width: "100%", height: "auto" }}
+				priority
 			/>
 			{language === "en" ? (
 				<p className="absolute top-[44%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-0 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl 2xl:text-6xl font-bold text-neutral-100 whitespace-nowrap drop-shadow-lg">
