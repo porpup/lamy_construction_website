ac34808a92153beda4df5bb7cc55c744bdf16597
Oleksandr Shyryayev <porpup@hotmail.com>
2024-07-21 22:25:24 -0400

    Update next-sitemap.config.js to exclude the URLs that redirect to external sites.

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ const nextConfig = {
 	images: {
 		unoptimized: true,
 	},
+	async redirects() {
+		return [
+			{
+				source: "/redirect-to-oiq", // Path to redirect from
+				destination: "https://www.oiq.qc.ca", // External URL
+				permanent: true,
+			},
+			{
+				source: "/redirect-to-oaq", // Path to redirect from
+				destination: "https://www.oaq.com", // External URL
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default nextConfig;
